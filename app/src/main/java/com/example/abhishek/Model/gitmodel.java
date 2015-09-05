5ffdf7a8665afdf1d91e04fca821829b9d2411a2
PuneetGupta123 <gup.123puneet@gmail.com>
2015-09-05 20:17:08 +0530

    First commit

---
+++ app/src/main/java/com/example/abhishek/Model/gitmodel.java
@@ -0,0 +1,5 @@
+package com.example.abhishek.Model;
+
+/**
+ * Created by dell on 8/22/2015.
+ */
---
+++ app/src/main/java/com/example/abhishek/api/gitapi.java
@@ -0,0 +1,7 @@
+package com.example.abhishek.api;
+
+/**
+ * Created by dell on 8/22/2015.
+ */
+public interface gitapi {
+}
